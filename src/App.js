06d80baef6852a89dbc66a27d14c60ad697b0771
Jean <20massalia@kakao.com>
2023-08-14 01:44:03 +0900

    기사 앱 기초 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import customAxios from './customAxios';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import DepartureLocation from "./components/DepartureLocation";
 import CheckPath from './components/CheckPath';
@@ -9,20 +8,6 @@ import SetArrivalLocation from './components/SetArrivalLocation';
 import LoginPage from './components/LoginPage';
 
 function App() {
-    // // IP주소 변수 선언
-    // const [ip, setIp] = useState('');
-
-    // // IP주소 값을 설정합니다.
-    // function callback(data) {
-    //   setIp(data);
-    // }
-  
-    // // 첫번째 렌더링을 다 마친 후 실행합니다.
-    // useEffect(() => {
-    //     // 클라이언트의 IP주소를 알아내는 백엔드의 함수를 호출합니다.
-    //     customAxios('/ip', callback);
-    //   }, []);
-
   return (
         <Routes>
           <Route path={"/"} element={<DepartureLocation />}></Route>
