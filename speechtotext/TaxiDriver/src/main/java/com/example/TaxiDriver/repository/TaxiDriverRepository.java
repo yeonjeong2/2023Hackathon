b06bbcb5711e98969e85b93577dc7d9422602321
민국 <121088189+X1n9fU@users.noreply.github.com>
2023-08-16 04:03:06 +0900

    Delete speechtotext directory

--- speechtotext/TaxiDriver/src/main/java/com/example/TaxiDriver/repository/TaxiDriverRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.TaxiDriver.repository;
-
-import com.example.TaxiDriver.entity.TaxiDriverEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaxiDriverRepository extends JpaRepository<TaxiDriverEntity, Long> {
-    TaxiDriverEntity findByPhone(String phone);
-}
